d75604a5eddeeb6ef81c2cddc9261ec319300066
brandonromero1555 <brandonromero1555@gmail.com>
2021-03-10 20:45:13 -0300

    recuperatorio

--- dos.js
+++ dos.js
@@ -1,4 +1,144 @@
 function mostrar()
 {
     alert("Ejercicio 2");
+
+
+  let nombre;
+  let carrera;
+  let sexo;
+  let cantidadMaterias;
+  let notaPromedio;
+  let edad;
+  let mejorNotaFisica;
+  let nombreMejorNotaFisica;
+  let flagMejorFisica = 1;
+  let flagMujerJoven = 1;
+  let nombreMujerJoven;
+  let edadMujerJoven;
+  let contTotalAlumnos = 0;
+  let contFisica = 0;
+  let contQuimica = 0;
+  let contSistemas = 0;
+  let porcentajeFisica; 
+  let porcentajeQuimica;
+  let porcentajeSistemas;
+  let flagMaterias = 1;
+  let masMaterias;
+  let nombreMasMaterias;
+  let edadMasMaterias
+
+    for ( let i = 0; i < 500 ; i++ ){
+
+        nombre = prompt("ingrese un nombre");
+        cantidadMaterias = parseInt(prompt("Ingrese cantidad de materias: "));
+		while ( cantidadMaterias <= 1  && cantidadMaterias >= 5){				
+			cantidadMaterias = prompt("Error. Ingrese cantidad de materias (maximo  5)");
+		}
+        carrera = prompt("Ingrese carrera: quimica, fisica y sistemas.");
+	while(carrera != "quimica" && carrera != "fisica" && carrera != "sistemas"){
+		carrera = prompt("Error. Ingrese carrera: ");
+	}
+    notaPromedio = parseInt(prompt("Ingrese nota promedio: "));
+    while (isNaN(notaPromedio) ||notaPromedio < 0  && notaPromedio >= 10){				
+        notaPromedio = prompt("Error. Ingrese nota promedio entre 0 y 10");
+    }
+    edad = prompt("ingrese su edad");
+	while (isNaN(edad) && edad <= 0)  {
+    edad = prompt("edad invalido, ingrese su edad");
+	}
+    sexo = prompt("Ingrese sexo f/m/nb: ");
+		while(sexo != "m" && sexo != "f" && sexo != "nb"){
+		sexo = prompt("Error. Ingrese el sexo: ");
+	   }
+
+
+if(carrera == "fisica" && (flagMejorFisica || notaPromedio > mejorNotaFisica)){
+    mejorNotaFisica = notaPromedio;
+    nombreMejorNotaFisica = nombre;
+    flagMejorFisica = 0;
+}
+
+
+    if(sexo == "f"){
+		if(flagMujerJoven || edad < edadMujerJoven){
+			edadMujerJoven = edad;
+			nombreMujerJoven = nombre;
+			
+			flagMujerJoven = 0;
+		}
+	}
+    
+
+    switch(carrera){
+        case "fisica":
+        contFisica++
+        break;
+        case "quimica":
+        contQuimica++
+        break;
+        case "sistemas":
+        contSistemas++
+        break;
+
+    }
+    contTotalAlumnos++
+   
+   
+    if(carrera != "quimica" && (flagMaterias == 1 || masMaterias < cantidadMaterias) ){
+        nombreMasMaterias = nombre;
+        masMaterias = cantidadMaterias;
+        edadMasMaterias = edad;
+        
+        flagMaterias = 0;
+
+    }
+
+
+
+   
+
+
+    }// TERMINA EL FOR
+
+    
+    if(flagMejorFisica == 0){
+        console.log("El nombre de la mejor nota de los alumnos de fisica es " + nombreMejorNotaFisica + " con una nota de " + mejorNotaFisica)
+    }else{
+        console.log("No se registraron alumnos de fisica");
+    }
+
+    
+    if(flagMujerJoven == 0){
+        console.log("La nombre de la mujer mas joven es " + nombreMujerJoven + " con una edad de " + edadMujerJoven + " años")
+    }else{
+        console.log("No se registraron mujeres")
+    }
+
+    porcentajeFisica = (contFisica * 100) / contTotalAlumnos
+    porcentajeQuimica = (contQuimica * 100) / contTotalAlumnos;
+    porcentajeSistemas = (contSistemas * 100) / contTotalAlumnos;
+    if(carrera == "fisica"){
+    console.log("El porcentaje de los que estudian fisica es " + porcentajeFisica)
+    }else{
+        console.log("No hay estudiantes de fisica.")
+    }
+    if(carrera == "quimica"){
+    console.log("El porcentaje de los que quimica es " + porcentajeQuimica)
+    }else{
+        console.log("No hay estudiantes de quimica.")
+    }   
+    if(carrera == "sistemas"){
+    console.log("El porcentaje de los que estudian sistemas es " + porcentajeSistemas)
+    }
+    else{
+        console.log("No hay estudiantes de sistemas.")
+    }
+
+
+   
+    if(flagMaterias = 0){
+        console.log("el estudiante que cursa mas materias  tiene " + edadMasMaterias +" años" +  " y se llama " +nombreMasMaterias)
+    }
+
+
 }
--- uno.js
+++ uno.js
@@ -2,4 +2,127 @@
 function mostrar()
 {
 	alert("Ejercicio 1");
+
+	let nombre;
+  let edad;
+  let sexo;
+  let puesto
+  let sueldo;
+  let respuesta;
+  let acumSueldoPro = 0;
+  let acumSueldoA = 0;
+  let acumSueldoQa = 0;
+  let contSueldoPro = 0;
+  let contSueldoA = 0;
+  let contSueldoQa = 0;
+  let promedioPro;
+  let promedioA;
+  let promedioQa;
+  let maxSueldo;
+  let flagMSueldo = 1;
+  let sexoMSueldo;
+  let flagMSMujer = 1;
+  let nombreMSMujer;
+  let sueldoMujer;
+  let contProNb = 0;
+
+  do {
+    nombre = prompt("ingrese nombre");
+
+    edad = parseInt(prompt("Edad:"));
+		while(isNaN(edad)){
+			edad = parseInt(prompt("Error. Ingrese una edad válida:"));
+    }
+    
+    sexo = prompt("ingrese sexo: f/m/nb")
+		while(sexo != "f" && sexo != "m" && sexo != "nb"){
+			sexo = prompt("error. ingrese sexo: f/m/nb");
+    }
+
+    puesto = prompt("ingrese puesto: programador/analista/Qa");
+		while(isNaN(puesto) && puesto != "programador" && puesto != "analista" && puesto != "Qa"){
+			puesto = prompt("error al ingresar puesto, ingrese puesto: programador/analista/Qa");
+    }
+
+    sueldo = parseInt(prompt("ingrese sueldo entre 15000 y 70000:"));
+		while(sueldo < 15000 || sueldo > 70000 || isNaN(sueldo)){
+			sueldo = parseInt(prompt("error al ingresar sueldo, entre 15000 y 70000"));
+    }
+
+    // Punto A
+    switch (puesto){
+      case "programador":
+      acumSueldoPro = sueldo;
+      contSueldoPro++;
+
+      break;
+      case "analista":
+      acumSueldoA = sueldo;
+      contSueldoA++;
+
+      break;
+      case "Qa":
+      acumSueldoQa = sueldo;
+      contSueldoQa++;
+
+      break;
+
+    }
+
+    //B
+   if(flagMSueldo || sueldo > maxSueldo){
+    maxSueldo = sueldo;
+    sexoMSueldo = sexo;
+    flagMSueldo = 0;
+   }
+
+   //C
+   if( sexo == "f" && (flagMSMujer || sueldo > sueldoMujer)){
+    sueldoMujer = sueldo;
+    nombreMSMujer = nombre;
+    flagMSMujer = 0;
+   }
+
+   //D
+   if(puesto == "programador" && sexo == "nb" && sueldo >= 20000 && sueldo <= 55000){
+
+    contProNb++;
+   }
+
+    respuesta = prompt("quiere ingresar otro empleado?");
+  } while (respuesta == "si");
+
+  // punto A
+  if(contSueldoPro != 0){
+    promedioPro = acumSueldoPro / contSueldoPro;
+
+    console.log("A sueldo promedio de programadores: " + promedioPro);
+  }
+
+  if(contSueldoA != 0){
+    promedioA = acumSueldoA / contSueldoA;
+
+    console.log("A sueldo promedio de analista: " + promedioA);
+  }
+  
+  if(contSueldoQa != 0){
+    promedioQa = acumSueldoQa / contSueldoQa;
+    console.log("A sueldo promedio de Qa: " + promedioQa);
+
+  }
+
+  //punto B
+  console.log("sexo con mayor sueldo: " + sexoMSueldo + " con un sueldo de: " + maxSueldo);
+
+  //punto C
+  console.log("el nombre del empleado femenino con mas sueldo es: " + nombreMSMujer);
+
+  //punto D
+  if(sexo == "nb" && puesto  == "programador"){ 
+    console.log("La cantidad de programadores no binarios es " + contProNb);
+
+}else{
+    console.log("No hay programadores no binarios");
+}
+
 }
